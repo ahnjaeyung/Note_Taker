4a825a4c3cabfc9d5566beeff3e99600fb8555ee
ahnjaeyung <ahn.jaeyung@gmail.com>
2022-01-23 13:52:38 -0600

    set up server.js file

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const path = require('path');
-const 
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const PORT = process.env.port || 3001;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
